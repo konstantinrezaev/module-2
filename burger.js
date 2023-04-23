7eca2e43c2f5d081beed249baf4aad74d0663acc
Konstantin Rezaev <rezaevky@yandex.ru>
2023-04-23 23:14:33 +0300

    creating a burger

---
+++ burger.js
@@ -0,0 +1,26 @@
+const BURGER_OPENED_CLASSNAME = "burgerOpen";
+const BURGER_BTN_OPENED_CLASSNAME = "burgerBtnOpen";
+const BODY_FIXED_CLASSNAME_2 = "bodyFixed";
+
+const bodyNode2 = document.querySelector("body");
+const burgerNode = document.querySelector(".jsBurger");
+const burgerBtnNode = document.querySelector(".jsBurgerBtn");
+const burgerContentNode = document.querySelector(".jsBurgerContent");
+
+burgerBtnNode.addEventListener("click", toggleBurger);
+
+burgerNode.addEventListener("click", (event) => {
+  const isClickOutsideContent = !event
+    .composedPath()
+    .includes(burgerContentNode);
+
+  if (isClickOutsideContent) {
+    toggleBurger();
+  }
+});
+
+function toggleBurger() {
+  burgerNode.classList.toggle(BURGER_OPENED_CLASSNAME);
+  bodyNode2.classList.toggle(BODY_FIXED_CLASSNAME_2);
+  burgerBtnNode.classList.toggle(BURGER_BTN_OPENED_CLASSNAME);
+}
